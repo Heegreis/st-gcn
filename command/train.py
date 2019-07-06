20d08cccc0c957086e4c76bbc82367dcc82d3b1b
root <run3113214@gmail.com>
2019-07-06 08:17:14 +0000

    set train.py for kinetics, ntu-xsub and ntu-xview

--- command/train.py
+++ command/train.py
@@ -3,8 +3,9 @@ import time
 
 tStart = time.time()
 
-# cmd = "python3 main.py recognition -c config/st_gcn/kinetics-skeleton/train.yaml --device 0"
-cmd = "python3 main.py recognition -c config/st_gcn/ntu-xsub/train.yaml --device 0 --batch_size 16 --test_batch_size 16"
+cmd = "python3 main.py recognition -c config/st_gcn/kinetics-skeleton/train.yaml"
+# cmd = "python3 main.py recognition -c config/st_gcn/ntu-xsub/train.yaml --device 0 --batch_size 16 --test_batch_size 16"
+# cmd = "python3 main.py recognition -c config/st_gcn/ntu-xview/train.yaml"
 os.system(cmd)
 
 tEnd = time.time()
