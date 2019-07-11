568c80e53d753b8a9abc0453c840e8ebc910fe3f
m10617028 <m10617028@gemail.yuntech.edu.tw>
2019-07-11 22:07:04 +0800

    combined difference net structs into one branch

--- command/train.py
+++ command/train.py
@@ -3,8 +3,9 @@ import time
 
 tStart = time.time()
 
+# --model net.st_gcn.Model --work_dir ./work_dir/recognition/ntu-xsub/ST_GCN --device 0 --batch_size 16 --test_batch_size 16
 cmd = "python3 main.py recognition -c config/st_gcn/kinetics-skeleton/train.yaml"
-# cmd = "python3 main.py recognition -c config/st_gcn/ntu-xsub/train.yaml --device 0 --batch_size 16 --test_batch_size 16"
+# cmd = "python3 main.py recognition -c config/st_gcn/ntu-xsub/train.yaml"
 # cmd = "python3 main.py recognition -c config/st_gcn/ntu-xview/train.yaml"
 os.system(cmd)
 
