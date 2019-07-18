bf31d377f074afa1c89db330051f8acffc2768f0
Heegreis <run3113214@gmail.com>
2019-07-18 14:03:15 +0000

    edit command/train.py

--- command/train.py
+++ command/train.py
@@ -6,6 +6,7 @@ tStart = time.time()
 # --model net.st_gcn.Model --work_dir ./work_dir/recognition/ntu-xsub/ST_GCN --device 0 --batch_size 16 --test_batch_size 16
 
 net_suffix = "stAddRes"
+train_version = ""
 date = 2
 # 1. ntu-xsub
 # 2. ntu-xview
@@ -13,6 +14,9 @@ date = 2
 
 if net_suffix != "":
     net_suffix = "_" + net_suffix
+
+if train_version != "":
+    train_version = "_" + train_version
 
 if date == 1:
     dataName_config = "ntu-xsub"
@@ -24,7 +28,7 @@ if date == 3:
     dataName_config = "kinetics-skeleton"
     dataName_workdir = "kinetics_skeleton"
 
-cmd = "python3 main.py recognition -c config/st_gcn/"+ dataName_config +"/train.yaml --model net.st_gcn"+ net_suffix +".Model --work_dir ./work_dir/recognition/"+ dataName_workdir +"/ST_GCN" + net_suffix
+cmd = "python3 main.py recognition -c config/st_gcn/"+ dataName_config +"/train.yaml --model net.st_gcn"+ net_suffix +".Model --work_dir ./work_dir/recognition/"+ dataName_workdir +"/ST_GCN" + net_suffix + train_version
 os.system(cmd)
 
 tEnd = time.time()
